b09714ba3a622847c9a3e99ed96a177403fb05c3
Seregy <seregy77@gmail.com>
2022-01-17 13:29:36 +0200

    Add calculation service test

---
+++ calculate-service/src/test/java/com/seregy77/k8s/calculation/CalculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seregy77.k8s.calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
